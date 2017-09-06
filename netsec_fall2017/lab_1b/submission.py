4864a8e142670c6ad616b67f4d5885b6e1f34462
Flyy-yu <peiyu94@gatech.edu>
2017-09-06 19:50:37 -0400

    1b

--- netsec_fall2017/lab_1b/submission.py
+++ netsec_fall2017/lab_1b/submission.py
@@ -1,6 +1,7 @@
 from playground.network.packet import PacketType
 from playground.network.packet.fieldtypes import UINT32, STRING, BUFFER, BOOL
 
+
 class RequestToConnect(PacketType):
     DEFINITION_IDENTIFIER = "lab1b.student.RequestToConnect"
     DEFINITION_VERSION = "1.0"
@@ -28,23 +29,24 @@ class Result(PacketType):
     DEFINITION_IDENTIFIER = "lab1b.student_x.result"
     DEFINITION_VERSION = "1.0"
     FIELDS = [
-        ("result", STRING)
+        ("result", BOOL)
     ]
 
 
 def basicUnitTest():
-
-
     packet1 = RequestToConnect()
     packet1Bytes = packet1.__serialize__()
     packet1a = RequestToConnect.Deserialize(packet1Bytes)
     assert packet1 == packet1a
+    print("Packet1 pass the Test")
+
     packet2 = NameRequest()
     packet2.Question = "What is your name?"
     packet2.ID = 1
     packet2Bytes = packet2.__serialize__()
     packet2a = NameRequest.Deserialize(packet2Bytes)
     assert packet2 == packet2a
+    print("Packet2 pass the Test")
 
     packet3 = AnswerNameRequest()
     packet3.Answer = "My name is Hello world"
@@ -52,12 +54,15 @@ def basicUnitTest():
     packet3Bytes = packet3.__serialize__()
     packet3a = AnswerNameRequest.Deserialize(packet3Bytes)
     assert packet3 == packet3a
+    print("Packet3 pass the Test")
 
     packet4 = Result()
-    packet4.result = "pass"
+    packet4.result = True
     packet4Bytes = packet4.__serialize__()
     packet4a = Result.Deserialize(packet4Bytes)
     assert packet4 == packet4a
+    print("Packet4 pass the Test")
+
 
 if __name__ == "__main__":
     basicUnitTest()
