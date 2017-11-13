b1aaac7928af50d2976174d0df389ac91827deef
linkinlzm <11963183+linkinlzm@users.noreply.github.com>
2017-11-12 21:15:51 -0500

    add signed cert

--- netsec_fall2017/lab2/src/lab2_protocol/CertFactory.py
+++ netsec_fall2017/lab2/src/lab2_protocol/CertFactory.py
@@ -1,8 +1,17 @@
 import os
-
+import codecs
+import random
 from playground.common.CipherUtil import *
 from cryptography import x509
 from cryptography.hazmat.primitives.asymmetric import rsa
+from Crypto.Cipher import AES
+from Crypto.Util import Counter
+import binascii
+import hashlib
+from binascii import a2b_hex, b2a_hex, hexlify
+from OpenSSL import crypto
+from binascii import a2b_hex, b2a_hex
+import base64
 
 root = os.path.dirname(os.path.abspath(__file__))
 path = os.path.dirname(os.path.dirname(root))
@@ -43,6 +52,50 @@ def getRootCert():
 #print(root)
 #print(path)
 #print(getPrivateKeyForAddr())
+
+
+key_bytes = 16
+
+def encrypt(key, plaintext):
+    print(len(key))
+    assert len(key) == key_bytes
+
+    # Choose a random, 16-byte IV.
+    iv = random.new().read(AES.block_size)
+
+    # Convert the IV to a Python integer.
+    iv_int = int(binascii.hexlify(iv), 16)
+
+    # Create a new Counter object with IV = iv_int.
+    ctr = Counter.new(AES.block_size * 8, initial_value=iv_int)
+
+    # Create AES-CTR cipher.
+    aes = AES.new(key, AES.MODE_CTR, counter=ctr)
+
+    # Encrypt and return IV and ciphertext.
+    ciphertext = aes.encrypt(plaintext)
+    return (iv, ciphertext)
+
+# Takes as input a 32-byte key, a 16-byte IV, and a ciphertext, and outputs the
+# corresponding plaintext.
+def decrypt(key, iv, ciphertext):
+    assert len(key) == key_bytes
+
+    # Initialize counter for decryption. iv should be the same as the output of
+    # encrypt().
+    iv_int = int(iv.encode('hex'), 16)
+    ctr = Counter.new(AES.block_size * 8, initial_value=iv_int)
+
+    # Create AES-CTR cipher.
+    aes = AES.new(key, AES.MODE_CTR, counter=ctr)
+
+    # Decrypt and return the plaintext.
+    plaintext = aes.decrypt(ciphertext)
+    return plaintext
+
+
+
+
 def main():
     # Loading a Certificate
     # rootCertificate = loadCertFromFile("root.crt")
@@ -53,26 +106,68 @@ def main():
     # Get subject details
     # Returns a dictionary, parse it to get individual fields
     # rootCertificateSubjectDetails = getCertSubject(rootCertificate)
-    '''cert = x509.load_pem_x509_certificate(getCert(), default_backend())
-    public_key = cert.public_key()
-    print(isinstance(public_key, rsa.RSAPublicKey))'''
+
+    '''crtObj = crypto.load_certificate(crypto.FILETYPE_PEM, getCert())
+    pubKeyObject = crtObj.get_pubkey()
+    pubKeyString = crypto.dump_publickey(crypto.FILETYPE_PEM, pubKeyObject)
+    print(pubKeyString)'''
+
+    Nc = random.getrandbits(64)
+    Ns = random.getrandbits(64)
+    PKc = random.getrandbits(128)
+    PKs = random.getrandbits(128)
+    shash = hashlib.sha1()
+    block = []
+    #block_0
+    shash.update("PLS1.0".encode('utf-8') + str(Nc).encode('utf-8') + str(Ns).encode('utf-8') + str(PKc).encode('utf-8') + str(PKs).encode('utf-8'))
+    block.append(shash.digest())
+    #block_1
+    shash.update(str(block[0]).encode('utf-8'))
+    block.append(shash.digest())
+    # block_2
+    shash.update(str(block[1]).encode('utf-8'))
+    block.append(shash.digest())
+    # block_3
+    shash.update(str(block[2]).encode('utf-8'))
+    block.append(shash.digest())
+    # block_4
+    shash.update(str(block[3]).encode('utf-8'))
+    block.append(shash.digest())
+    for bl in block:
+        print(bl)
+
+    block_bytes = hexlify(block[0] + block[1] + block[2] + block[3] + block[4])
+    block_bits = bin(int(block_bytes,16))
+    print(len(block_bits))
+    print(type(block_bits))
+    print(block_bits)
+    Ekc = block_bits[0:128]
+    print(len(Ekc))
+    print(Ekc)
 
 
-    cert = getCertFromBytes(getCert())
+
+    plaintext = "this is a text message"
+
+    '''cert = getCertFromBytes(getCert())
     public_key = cert.public_key()
     iv = os.urandom(16)
     print(isinstance(public_key, rsa.RSAPublicKey))
     print(getCertIssuer(cert))
     print(getCertSubject(cert))
-    enc = CIPHER_AES128_CBC(public_key, iv)
-    ciphertext = enc.encrypt("test message")
-    print(ciphertext)
+    zeroKey = "\x00" * 16  # 16 bytes of 0
+    zeroIv = "\x00" * 16'''
+
+    #(iv, ciphertext) = encrypt(pubKeyString, 'hella')
+    #print(decrypt(pubKeyString, iv, ciphertext))
+
+
+
 
 
 
 if __name__ == "__main__":
     main()
-
 
 
 
