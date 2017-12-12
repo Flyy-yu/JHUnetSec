da77826d832429299706fd569c7e57555b9bf258
Flyy-yu <peiyu94@gatech.edu>
2017-12-11 22:00:57 -0500

    eva

--- netsec_fall2017/lab3/src/lab3_protocol/eva.py
+++ netsec_fall2017/lab3/src/lab3_protocol/eva.py
@@ -2,19 +2,26 @@ from .lab3a import *
 
 
 class eavesdrop(asyncio.Protocol):
+    def __init__(self):
+        self.eavesdrop = playground.network.protocols.switching.PlaygroundSwitchTxProtocol(demux, "20174.*.*.*")
+
     def connectionMade(self):
         pass
 
+    def demux(self, src, srcPort, dst, dstPort, demuxData):
+        d = PacketBaseType.Deserializer()
+        d.update(demuxData)
+        for pkt in d.nextPackets():
+            print(pkt)
 
-def demux(self, src, srcPort, dst, dstPort, demuxData):
-    d = PacketBaseType.Deserializer()
-    d.update(demuxData)
-    for pkt in d.nextPackets():
-        print(pkt)
+    def start(self):
+        coro = asyncio.get_event_loop().create_connection(lambda: self.eavesdrop, "192.168.200.240", "9090")
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(coro)
+        loop.run_forever()
+        loop.close()
 
-    
 
-
-eavesdrop = playground.network.protocols.switching.PlaygroundSwitchTxProtocol(demux, "20174.*.*.*")
-asyncio.get_event_loop().create_connection(lambda: eavesdrop, "192.168.200.240", "9090")
-
+if __name__ == '__main__':
+    eva = eavesdrop()
+    eva.start()
