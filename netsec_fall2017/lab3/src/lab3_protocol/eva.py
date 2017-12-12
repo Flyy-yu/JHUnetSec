47c5ed25c60bebda848858018b122ac160e7d187
Flyy-yu <peiyu94@gatech.edu>
2017-12-11 22:27:20 -0500

    eva

--- netsec_fall2017/lab3/src/lab3_protocol/eva.py
+++ netsec_fall2017/lab3/src/lab3_protocol/eva.py
@@ -1,25 +1,26 @@
 from .lab3a import *
+
 logging.getLogger().setLevel(logging.NOTSET)  # this logs *everything*
 logging.getLogger().addHandler(logging.StreamHandler())  # logs to stderr
 
+
 class eavesdrop(asyncio.Protocol):
     def __init__(self):
-
         self._discoveryConnection = playground.network.protocols.switching.PlaygroundSwitchTxProtocol(self,
                                                                                                       "20174.*.*.*")
+
     def start(self):
-        coro = asyncio.get_event_loop().create_connection(lambda: self._discoveryConnection, "192.168.200.240", 9090)
+        coro = asyncio.get_event_loop().create_connection(lambda: self._discoveryConnection, "192.168.200.240", "9090")
         asyncio.get_event_loop().create_task(coro)
 
     def connectionMade(self):
         pass
 
     def demux(self, src, srcPort, dst, dstPort, demuxData):
-
         d = PacketType.Deserializer()
         d.update(demuxData)
-        pkts = list(d.nextPackets())
-        print(pkts)
+        for pkt in d.nextPackets():
+            print(pkt)
 
 
 if __name__ == '__main__':
