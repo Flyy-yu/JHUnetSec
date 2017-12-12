d10388d61b482adbf8c83201d77e369152c3b7a1
Flyy-yu <peiyu94@gatech.edu>
2017-12-11 22:13:27 -0500

    eva

--- netsec_fall2017/lab3/src/lab3_protocol/eva.py
+++ netsec_fall2017/lab3/src/lab3_protocol/eva.py
@@ -6,10 +6,7 @@ class eavesdrop(asyncio.Protocol):
 
         self._discoveryConnection = playground.network.protocols.switching.PlaygroundSwitchTxProtocol(self,
                                                                                                       "20174.*.*.*")
-
     def start(self):
-        if self._running: return
-        self._running = True
         coro = asyncio.get_event_loop().create_connection(lambda: self._discoveryConnection, "192.168.200.240", 9090)
         asyncio.get_event_loop().create_task(coro)
 
