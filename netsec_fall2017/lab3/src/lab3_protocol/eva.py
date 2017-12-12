0caf36f11db7f09bb7932841b40296a4774b0372
Flyy-yu <peiyu94@gatech.edu>
2017-12-11 22:12:54 -0500

    eva

--- netsec_fall2017/lab3/src/lab3_protocol/eva.py
+++ netsec_fall2017/lab3/src/lab3_protocol/eva.py
@@ -3,24 +3,26 @@ from .lab3a import *
 
 class eavesdrop(asyncio.Protocol):
     def __init__(self):
-        pass
-    
+
+        self._discoveryConnection = playground.network.protocols.switching.PlaygroundSwitchTxProtocol(self,
+                                                                                                      "20174.*.*.*")
+
+    def start(self):
+        if self._running: return
+        self._running = True
+        coro = asyncio.get_event_loop().create_connection(lambda: self._discoveryConnection, "192.168.200.240", 9090)
+        asyncio.get_event_loop().create_task(coro)
+
     def connectionMade(self):
         pass
 
     def demux(self, src, srcPort, dst, dstPort, demuxData):
-        d = PacketBaseType.Deserializer()
-        d.update(demuxData)
-        for pkt in d.nextPackets():
-            print(pkt)
 
-    def start(self):
-        self.eavesdrop = playground.network.protocols.switching.PlaygroundSwitchTxProtocol(self.demux, "20174.*.*.*")
-        coro = asyncio.get_event_loop().create_connection(lambda: self.eavesdrop, "192.168.200.240", "9090")
-        loop = asyncio.get_event_loop()
-        loop.run_until_complete(coro)
-        loop.run_forever()
-        loop.close()
+        d = PacketType.Deserializer()
+        d.update(demuxData)
+        pkts = list(d.nextPackets())
+        if not pkts: return
+        self._discoveryConnection.write(dst, dstPort, src, srcPort)
 
 
 if __name__ == '__main__':
