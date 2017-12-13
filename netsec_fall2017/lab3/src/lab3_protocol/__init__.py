89875bf7acf66a1fff5f5b9b360999ad4aa72850
Flyy-yu <peiyu94@gatech.edu>
2017-12-13 00:23:41 -0500

    pls

--- netsec_fall2017/lab3/src/lab3_protocol/__init__.py
+++ netsec_fall2017/lab3/src/lab3_protocol/__init__.py
@@ -6,4 +6,4 @@ myPeepConnector = playground.Connector(protocolStack=(
     PeepServerFactory()))
 
 playground.setConnector("lab3_protocol", myPeepConnector)
-playground.setConnector("my_team_lab3_protocol", myPeepConnector)
+playground.setConnector("pls", myPeepConnector)
