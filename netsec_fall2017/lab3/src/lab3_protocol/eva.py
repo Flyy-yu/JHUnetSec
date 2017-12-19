15567e48b518ffffd6e0b8ac4c4af2fdcb2ee44a
Flyy-yu <peiyu94@gatech.edu>
2017-12-19 03:47:07 -0500

    eva

--- netsec_fall2017/lab3/src/lab3_protocol/eva.py
+++ netsec_fall2017/lab3/src/lab3_protocol/eva.py
@@ -50,17 +50,16 @@ class eavesdrop(asyncio.Protocol):
         for pkt in d.nextPackets():
             if isinstance(pkt, PEEPPacket):
                 e.update(pkt.Data)
-
                 for pkt in e.nextPackets():
                     print("this packet is:" + pkt.DEFINITION_IDENTIFIER)
                     if (isinstance(pkt, SessionOpen)):
                         print("account:")
                     print(pkt.Account)
-                if (isinstance(pkt, OpenSession)):
-                    print("login:")
-                    print(pkt.Login)
-                    print("passwordhash")
-                    print(pkt.PasswordHash)
+                    if (isinstance(pkt, OpenSession)):
+                        print("login:")
+                        print(pkt.Login)
+                        print("passwordhash")
+                        print(pkt.PasswordHash)
 
 
 if __name__ == '__main__':
