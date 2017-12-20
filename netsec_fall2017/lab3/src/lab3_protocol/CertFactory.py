ed4c332ea836fda84aa488428afc98181b4165f5
Flyy-yu <peiyu94@gatech.edu>
2017-12-19 22:03:41 -0500

    cert

--- netsec_fall2017/lab3/src/lab3_protocol/CertFactory.py
+++ netsec_fall2017/lab3/src/lab3_protocol/CertFactory.py
@@ -1,22 +1,32 @@
 import os
+
 root = os.path.dirname(os.path.abspath(__file__))
 path = os.path.dirname(os.path.dirname(root))
+
+
 def getPrivateKeyForAddr(addr):
-    if addr == "20174.1.6666.1":
+    if addr == "20174.1.2333.1":
         with open(path + "/certs/server.key") as fp:
             server_key = fp.read()
         fp.close()
         return server_key
-    if addr == "20174.1.6666.2":
+    if addr == "20174.1.2333.2":
         with open(path + "/certs/client.key") as fp:
             client_key = fp.read()
         fp.close()
         return client_key
+    if addr == "20174.1.2333.2333":
+        with open(path + "/certs/prikey2333") as fp:
+            client_key = fp.read()
+        fp.close()
+        return client_key
+
     return None
+
 
 def getCertsForAddr(addr):
     certs = []
-    if addr == "20174.1.6666.1":
+    if addr == "20174.1.2333.1":
         with open(path + "/certs/server.cert", "rb") as fp:
             certs.append(fp.read())
         fp.close()
@@ -24,7 +34,7 @@ def getCertsForAddr(addr):
             certs.append((fp.read()))
         fp.close()
         return certs
-    if addr == "20174.1.6666.2":
+    if addr == "20174.1.2333.2":
         with open(path + "/certs/client.cert", "rb") as fp:
             certs.append(fp.read())
         fp.close()
@@ -32,7 +42,24 @@ def getCertsForAddr(addr):
             certs.append((fp.read()))
         fp.close()
         return certs
+    if addr == "20174.1.2333.2333":
+        with open(path + "/certs/client2333.cert", "rb") as fp:
+            certs.append(fp.read())
+        fp.close()
+        with open(path + "/certs/addr2333.cert", "rb") as fp:
+            certs.append((fp.read()))
+        fp.close()
+        return certs
+    if addr == "20174.1.1337.3":
+        with open(path + "/certs/client636.cert", "rb") as fp:
+            certs.append(fp.read())
+        fp.close()
+        with open(path + "/certs/addr636.cert", "rb") as fp:
+            certs.append((fp.read()))
+        fp.close()
+        return certs
     return None
+
 
 def getRootCert():
     with open(path + "/certs/root.crt", "rb") as fp:
@@ -42,33 +69,33 @@ def getRootCert():
 
 
 
-# import os
-#
-# root = os.path.dirname(os.path.abspath(__file__))
-# path = os.path.dirname(os.path.dirname(root))
-#
-#
-# def getPrivateKeyForAddr(addr):
-#     with open(path + "/certs/server.key") as fp:
-#         server_key = fp.read()
-#     fp.close()
-#     return server_key
-#
-#
-# def getCertsForAddr(addr):
-#     certs = []
-#
-#     with open(path + "/certs/server.cert", "rb") as fp:
-#         certs.append(fp.read())
-#     fp.close()
-#     with open(path + "/certs/signed.cert", "rb") as fp:
-#         certs.append((fp.read()))
-#     fp.close()
-#     return certs
-#
-#
-# def getRootCert():
-#     with open(path + "/certs/root.crt", "rb") as fp:
-#         cert = fp.read()
-#     fp.close()
-#     return cert
+    # import os
+    #
+    # root = os.path.dirname(os.path.abspath(__file__))
+    # path = os.path.dirname(os.path.dirname(root))
+    #
+    #
+    # def getPrivateKeyForAddr(addr):
+    #     with open(path + "/certs/server.key") as fp:
+    #         server_key = fp.read()
+    #     fp.close()
+    #     return server_key
+    #
+    #
+    # def getCertsForAddr(addr):
+    #     certs = []
+    #
+    #     with open(path + "/certs/server.cert", "rb") as fp:
+    #         certs.append(fp.read())
+    #     fp.close()
+    #     with open(path + "/certs/signed.cert", "rb") as fp:
+    #         certs.append((fp.read()))
+    #     fp.close()
+    #     return certs
+    #
+    #
+    # def getRootCert():
+    #     with open(path + "/certs/root.crt", "rb") as fp:
+    #         cert = fp.read()
+    #     fp.close()
+    #     return cert
